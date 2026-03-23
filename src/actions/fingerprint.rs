1fdbe349813a24e52fbd30cdfcd0147e7a78b8d7
RoyPrinsGH <royprinsgm@gmail.com>
2026-03-23 20:59:34 +0100

    Rename trust.rs to hash.rs and add trust_manifest.rs

--- src/actions/fingerprint.rs
+++ src/actions/fingerprint.rs
@@ -1,10 +1,10 @@
 use crate::errors::JaoResult;
-use crate::trust;
+use crate::hash;
 use std::io::{self, Write};
 use std::path::Path;
 
 pub fn fingerprint_script(script_path: impl AsRef<Path>) -> JaoResult<()> {
-    let (_, fingerprint) = trust::fingerprint_file(script_path)?;
+    let (_, fingerprint) = hash::fingerprint_file(script_path)?;
     let mut out = io::stdout().lock();
 
     writeln!(out, "{fingerprint}")?;
---
+++ src/hash.rs
@@ -0,0 +1,16 @@
+use crate::errors::JaoResult;
+use sha2::{Digest, Sha256};
+use std::path::{Path, PathBuf};
+
+pub fn fingerprint_file(path: impl AsRef<Path>) -> JaoResult<(PathBuf, String)> {
+    let canonical_path = std::fs::canonicalize(path)?;
+    let file_contents = std::fs::read(&canonical_path)?;
+
+    let mut hasher = Sha256::new();
+
+    hasher.update(canonical_path.to_string_lossy().as_bytes());
+    hasher.update([0]);
+    hasher.update(file_contents);
+
+    Ok((canonical_path, format!("{:x}", hasher.finalize())))
+}
